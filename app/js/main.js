3a37c313974cb834db4df29cddb1e391af7ecc33
lasiuchenko <yaroslav.lasjuchenko@gmail.com>
2021-11-26 10:25:13 +0100

    added styles hover

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,8 @@ $(function () {
         dots: false,
         infinite: false,
         slidesToShow: 1,
-        prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/icon-arrow-left.svg" alt="Левая стрелка"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icon-arrow-right.svg" alt="Правая стрелка"></button>'
+        // prevArrow: '<button type="button" class="slick-prev"></button>',
+        // nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icon-arrow-right.svg" alt="Правая стрелка"></button>'
 
       }
 
