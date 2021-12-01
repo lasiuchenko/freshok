ff1832f20627ff40a9c6c4461d22211f6151b8e8
lasiuchenko <yaroslav.lasjuchenko@gmail.com>
2021-12-01 15:40:28 +0100

    changed top-slidert buttons

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,23 @@
 $(function () {
-    $('.top-slider__list').slick( {
-        dots: false,
-        infinite: false,
-        slidesToShow: 1,
-        // prevArrow: '<button type="button" class="slick-prev"></button>',
-        // nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icon-arrow-right.svg" alt="Правая стрелка"></button>'
+    $('.top-slider__list').slick({
+      dots: false,
+      infinite: false,
+      slidesToShow: 1,
+      prevArrow: '<button type="button" class="top-slider__arrow-btn top-slider__arrow-btn--prev"><svg class="top-slider__arrow"><use xlink:href="images/sprite.svg#icon-arrow-left"></use></svg></button>',
+      nextArrow: '<button type="button" class="top-slider__arrow-btn top-slider__arrow-btn--next"><svg class="top-slider__arrow"><use xlink:href="images/sprite.svg#icon-arrow-right"></use></svg></button>'
 
-      }
+    });
+  
+  $('.catalog__btn').on('click', function () {
+    $('.catalog__btn').toggleClass('catalog__btn--active');
+     $('.catalog__list').toggle();
+  });
 
-    );
+
     $(".star").rateYo({
       readOnly: true,
     });
- 
+  
     var mixer=mixitup('.products__content');
   }
-
 )
