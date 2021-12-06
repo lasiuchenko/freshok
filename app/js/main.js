0f6311b6ae61784403ee12d77171e9128bfd1be8
lasiuchenko <yaroslav.lasjuchenko@gmail.com>
2021-12-06 23:17:55 +0100

    changed

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,17 @@ $(function () {
   $('.top-slider__list').slick({
     dots: false,
     infinite: false,
-    slidesToShow: 1,
     prevArrow: '<button type="button" class="top-slider__arrow-btn top-slider__arrow-btn--prev"><svg class="top-slider__arrow"><use xlink:href="images/sprite.svg#icon-arrow-left"></use></svg></button>',
     nextArrow: '<button type="button" class="top-slider__arrow-btn top-slider__arrow-btn--next"><svg class="top-slider__arrow"><use xlink:href="images/sprite.svg#icon-arrow-right"></use></svg></button>'
 
+  });
+
+  $('.partners__list').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4, 
+    slidesToScroll: 4
   });
   
   $('.catalog__btn').on('click', function () {
@@ -18,8 +25,9 @@ $(function () {
   });
 
   $('.cart-popup__close-btn').on('click', function () {
-    $('.cart-popup__item').hide();
-  }); // как связать кнопку с блоков, сейчас кнопка в первой карточке адаляет все карточки
+     let parent = $(this).parent();
+  parent.hide();
+  });
 
   $('.rate__star').rateYo({
     readOnly: true,
@@ -29,27 +37,27 @@ $(function () {
   });
 
   $('.plus-btn, .minus-btn').on('click', function () {
-      var $input = $(this).parents('.cart-popup__quantity').find('.cart-popup__input');
+    let $input = $(this).parents('.cart-popup__quantity').find('.cart-popup__input');
     if($(this).hasClass('minus-btn')) {
-      var count = parseFloat($input.val()) - 1;
+      let count = parseFloat($input.val()) - 1;
       count = count < 1 ? 1 : count;
       
       $input.val(count);
     }
     else {
-      var count = parseFloat($input.val()) + 1
+    let count = parseFloat($input.val()) + 1
       $input.val(count);
     }
   
 });
   
-  var mixerOne = mixitup('.products', {
+  let mixerOne = mixitup('.products', {
     controls: {
     scope: 'local'
     }
   });  
 
-  var mixerTwo = mixitup('.discounts', {
+  let mixerTwo = mixitup('.discounts', {
     controls: {
     scope: 'local'
     }
